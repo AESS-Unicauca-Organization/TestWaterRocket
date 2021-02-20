60ebda6b4a570e201d7de228b9be78be86dc8124
cfrealpe <cfrealpe@unicauca.edu.co>
2021-02-19 22:42:53 -0500

    Imrpoved async of data. Queue unimplemented yet

--- RocketLoggerArduino/RocketLoggerArduino.ino
+++ RocketLoggerArduino/RocketLoggerArduino.ino
@@ -8,22 +8,22 @@
 #include "Adafruit_BMP280.h"
 #include "MPU9250_asukiaaa.h"
 #include <SD.h>
+#include <ArduinoQueue.h>
 
-#define INTERVAL 100 // ms
-
-struct LoggerObject { // 10 fields of float
-  float acc[3]; // X,Y,Z
-  float gyro[3];
-  float mag[3];
-  float pressure;
-};
+#define BMP_INTERVAL 100 // ms
 
 File logFile;
 MPU9250_asukiaaa mySensor;
-float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
 
 Adafruit_BMP280 bmp; // use I2C interface
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+struct BufferObject {
+    char *keyword[2]; // Can be: aX,aY,aZ,gX,gY,gZ,mX,mY,mZ,p
+    float value;
+};
+
+ArduinoQueue<BufferObject> bufferQueue(100, 1024);
 
 void setup() {
   Wire.begin();
@@ -55,43 +55,51 @@ void setup() {
 }
 
 void loop() {
-  if (mySensor.accelUpdate() == 0 && mySensor.gyroUpdate() == 0 & mySensor.magUpdate() == 0) {
-        static uint32_t prev_ms = millis();
-        if (millis() > prev_ms + INTERVAL) {
+  if (mySensor.accelUpdate() == 0) {
+    writeToSD("accX", mySensor.accelX());
+    writeToSD("accY", mySensor.accelY());
+    writeToSD("accZ", mySensor.accelZ());
+  }
+
+  if (mySensor.gyroUpdate() == 0) {
+    writeToSD("gyrX", mySensor.gyroX());
+    writeToSD("gyrY", mySensor.gyroY());
+    writeToSD("gyrZ", mySensor.gyroZ());
+  }
+
+  if (mySensor.magUpdate() == 0) {
+    writeToSD("magX", mySensor.magX());
+    writeToSD("magY", mySensor.magY());
+    writeToSD("magZ", mySensor.magZ());
+  }
+  
+  static uint32_t prev_ms = millis();
+  if (millis() > prev_ms + BMP_INTERVAL) {
           
-            sensors_event_t pressure_event;
-            bmp_pressure->getEvent(&pressure_event);
+      sensors_event_t pressure_event;
+      bmp_pressure->getEvent(&pressure_event);
 
-            LoggerObject data = {
-              {mySensor.accelX(), mySensor.accelY(), mySensor.accelZ()},
-              {mySensor.gyroX(), mySensor.gyroY(), mySensor.gyroZ()},
-              {mySensor.magX(), mySensor.magY(), mySensor.magZ()},
-              (float) pressure_event.pressure,
-            };
+      float pressure = (float) pressure_event.pressure;
+      writeToSD("prs", pressure);
+            
+      prev_ms = millis();
+  }
+}
 
-            // Abrir archivo y escribir valor
-            logFile = SD.open("datalog.txt", FILE_WRITE);
-            
-            if (logFile) { 
-                  logFile.print("ms:");
-                  logFile.print(millis());
-                  logFile.println(data.acc[0]);
-                  logFile.println(data.acc[1]);
-                  logFile.println(data.acc[2]);
-                  logFile.println(data.gyro[0]);
-                  logFile.println(data.gyro[1]);
-                  logFile.println(data.gyro[2]);
-                  logFile.println(data.mag[0]);
-                  logFile.println(data.mag[1]);
-                  logFile.println(data.mag[2]);
-                  logFile.println(data.pressure);
-                  
-                  logFile.close();
-            } else {
-              digitalWrite(13, HIGH);
-            }
-            
-            prev_ms = millis();
-        }
-    }
+// Writing to SD can take up to 200ms
+void writeToSD(String keyword, float value) {
+  // Abrir archivo y escribir valor
+  logFile = SD.open("datalog.txt", FILE_WRITE);
+      
+  if (logFile) { 
+      logFile.println(millis()); // ms
+      logFile.print(",");
+      logFile.print(keyword);
+      logFile.print(",");
+      logFile.print(value);
+      
+      logFile.close();
+  } else {
+    digitalWrite(13, HIGH);
+  }
 }
